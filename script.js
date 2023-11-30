a9f32366fb376640ea02038ab86538d4adb0f204
JJeshua <jjeshuac@outlook.com>
2023-11-29 21:29:38 -0800

    Added dark mode toggler.

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
+let darkModeToggled = false;
+const darkModeTogglerIconPath = 'assets/icons/dark-mode-toggler.svg';
+const lightModeTogglerIconPath = 'assets/icons/light-mode-toggler.svg';
+
 const switchTheme = () => {
-  document.documentElement.setAttribute('data-theme', 'dark');
+  const newTheme = darkModeToggled ? 'light' : 'dark';
+  toggleButton.src = darkModeToggled
+    ? darkModeTogglerIconPath
+    : lightModeTogglerIconPath;
+  darkModeToggled = !darkModeToggled;
+  document.documentElement.setAttribute('data-theme', newTheme);
 };
 
+const toggleButton = document.getElementById('dark-mode-toggler-button');
+toggleButton.addEventListener('click', switchTheme);
+
+// sets initial state of data-theme
 switchTheme();
